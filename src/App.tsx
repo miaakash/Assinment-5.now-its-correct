117301eb6040baeeea012c292a20e0bc4bd1b59a
miaakash <akashmia5740@gmail.com>
2026-09-14 07:30:03 +0600

    7th comit

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,13 @@ function App() {
   
   return (
     <>
-     
+     <Nav></Nav>
+     <Hero></Hero>
+      
+      <Suspense fallback = {<h2 className='text-center'> Loading ......</h2>}>
+         <Technology technologyPromise  = {technologyPromise}></Technology>
+      </Suspense>
+      <ToastContainer></ToastContainer>
       
     </>
   )
