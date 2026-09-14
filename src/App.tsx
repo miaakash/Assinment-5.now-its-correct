5d56ed32f26d6428add13789d10db21aec1d7664
miaakash <akashmia5740@gmail.com>
2026-09-14 07:31:06 +0600

    8th comit

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,8 @@ function App() {
          <Technology technologyPromise  = {technologyPromise}></Technology>
       </Suspense>
       <ToastContainer></ToastContainer>
+
+      <Footer></Footer>
       
     </>
   )
